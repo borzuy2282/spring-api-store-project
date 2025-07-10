78939b53c1ac70ad8f85d3e31c728c9516c4a7a9
Tymofii <tim.barzashvili@icloud.com>
2025-07-10 23:32:10 +0200

    Project 2 step 9

---
+++ src/main/java/com/codewithmosh/store/dtos/OrderDto.java
@@ -0,0 +1,17 @@
+package com.codewithmosh.store.dtos;
+
+import com.codewithmosh.store.entities.OrderStatus;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Data
+public class OrderDto {
+    private Long id;
+    private String  status;
+    private LocalDateTime createdAt;
+    private Set<OrderItemDto> items;
+    private BigDecimal totalPrice;
+}
